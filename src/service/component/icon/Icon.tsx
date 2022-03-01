c2187638b08782c03f8df3ed573af74ea87ba5e5
Kang Gyeong Seok <rkdrudtjr159@gmail.com>
2022-03-01 15:11:03 +0900

    [New feature] Icon 컴포넌트 구현

---
+++ src/service/component/icon/Icon.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+
+import Image from 'next/image'
+
+export interface IconProp {
+  url: string
+  width?: number
+  height?: number
+  className?: string
+  priority?: boolean
+
+  onClick?: () => void
+}
+
+export const Icon = ({
+  url,
+  width = 24,
+  height = 24,
+  className,
+  priority,
+  onClick,
+}: IconProp) => {
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        objectFit: 'contain',
+      }}
+    >
+      <Image src={url} width={width} height={height} priority={priority} />
+    </div>
+  )
+}
