b5fc7e771009bdcf9f9009e2c8ee0619bbc4e71e
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2022-03-03 17:00:10 +0900

    [New feature] soldout 일때 이미지 덮어쓰기 추가

--- src/component/card/GoodCard.tsx
+++ src/component/card/GoodCard.tsx
@@ -3,6 +3,7 @@ import ImgWithEmpty from "../image/ImgWithEmpty";
 import React from "react";
 import { getNumberWithComma } from "../../utils/getNumberWithComma";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 interface GoodCardProp {
   good: Good;
@@ -15,38 +16,38 @@ const GoodCard = ({ good, className = "" }: GoodCardProp) => {
   const goGoodLink = () => {
     router.push(good.linkUrl);
   };
+
   return (
-    <div
-      className={`h-[366px] w-[188px] cursor-pointer ${className}`}
-      onClick={goGoodLink}
-    >
+    <div className={`h-[366px] w-[188px] cursor-pointer ${className}`} onClick={goGoodLink}>
       <div className="h-[240px]">
-        <ImgWithEmpty className="h-[226px] w-[188px]" url={good.imageUrl} />
-        <div className="relative top-[-18px] left-[9px] flex justify-start">
-          {good.isExclusive && (
-            <span className="inline-flex bg-[#18A286] p-2 text-xs text-white">
-              단독
-            </span>
-          )}
+        <div className="relative">
+          <ImgWithEmpty
+            className={`h-[226px] w-[188px] ${good.isSoldOut && "bg-saleOut opacity-20"}`}
+            url={good.imageUrl}
+          />
+          <div className="relative top-[-18px] left-[9px] flex min-h-[32px] justify-start">
+            {good.isExclusive ? (
+              <span className="inline-flex bg-[#18A286] p-2 text-xs text-white">단독</span>
+            ) : (
+              <span className="inline-flex p-2 text-xs text-white"></span>
+            )}
+          </div>
         </div>
+        {good.isSoldOut && (
+          <div className="relative top-[-258px] flex h-[226px] w-[188px] items-center justify-center">
+            <Image src={"/icon/soldout_text.png"} layout="fixed" width={120} height={22} />
+          </div>
+        )}
       </div>
       <div className="flex flex-col justify-start p-1 text-left">
         <span className="text-[11px]">{good.brandName}</span>
-        <span className="min-h-[40px] text-left text-sm font-bold line-clamp-2">
-          {good.goodsName}
-        </span>
+        <span className="min-h-[40px] text-left text-sm font-bold line-clamp-2">{good.goodsName}</span>
         <div className="flex justify-between">
-          <span className="font-bold">{`${getNumberWithComma(
-            good.price
-          )}원`}</span>
-          {good.isSale && (
-            <span className="text-[#FF0000]">{`${good.saleRate}%`}</span>
-          )}
+          <span className="font-bold">{`${getNumberWithComma(good.price)}원`}</span>
+          {good.isSale && <span className="text-[#FF0000]">{`${good.saleRate}%`}</span>}
         </div>
         {good.isSale && (
-          <span className="flex text-xs text-oldgray6 line-through">{`${getNumberWithComma(
-            good.normalPrice
-          )}원`}</span>
+          <span className="flex text-xs line-through text-oldgray6">{`${getNumberWithComma(good.normalPrice)}원`}</span>
         )}
       </div>
     </div>
